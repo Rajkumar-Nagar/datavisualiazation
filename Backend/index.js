42b763b3c1db01bf1c5fc692310a6a891ae20912
Rajkumar Nagar <rajkumarnagar264@gamil.com>
2024-04-24 02:03:28 +0530

    api

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,11 @@
 import { app } from "./app.js"
 import connectDB from "./db.js"
+import dotenv from "dotenv"
 
+
+dotenv.config({
+    path: "./.env"
+})
 connectDB().then(()=>{
     app.listen(3000,()=>{
         console.log(`server is running on 3000`)
